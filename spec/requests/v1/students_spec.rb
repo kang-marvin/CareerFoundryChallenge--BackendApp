b9fcd2a5858eb93761512d4159f68ae7cf92130e
Marvin Kang'ethe <marvin.kangethe@andela.com>
2020-01-23 16:47:27 +0300

    Add tests for Student Create Appointment

--- spec/requests/v1/students_spec.rb
+++ spec/requests/v1/students_spec.rb
@@ -36,7 +36,7 @@ RSpec.describe 'Student Requests', type: :request do
     before {
       get '/api/v1/users/students/show',
           headers: invalid_headers(),
-          params: { id: student.id }
+          params: { student_id: student.id }
     }
 
     it 'returns the requested student' do
@@ -48,11 +48,40 @@ RSpec.describe 'Student Requests', type: :request do
     before {
       get '/api/v1/users/students/show',
           headers: invalid_headers(),
-          params: { id: 0 }
+          params: { student_id: 0 }
     }
 
     it 'returns list of errors' do
       expect(json_response[:errors]).to_not be_empty
+    end
+  end
+
+  context 'When students updates appointment details ie title' do
+    let!(:mentor)   { create(:mentor, email: 'mentor@careerfoundry.com' ) }
+    let!(:student)  { create(:student, email: 'student@careerfoundry.com' ) }
+
+    time_now = DateTime.now
+
+    params = {
+      title: 'My appointment slot',
+      description: '',
+      start_time: time_now,
+      end_time: time_now + Appointment::MINIMUM_SLOT_TIME
+    }
+
+    before {
+      post '/api/v1/users/students/create_appointment',
+          headers: invalid_headers(),
+          params: params
+                    .merge({
+                      mentor_id: mentor.id,
+                      student_id: student.id
+                    })
+                    .to_json
+    }
+
+    it 'returns the created appointment object' do
+      expect(json_response[:appointment][:status]).to eql('pending')
     end
   end
 
